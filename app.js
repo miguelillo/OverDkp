4b436f3c4794464523193dc6e9aa8b1b758df7c1
Miguel Ángel Navarro Vera <manavarro@encamina.com>
2019-11-04 21:48:56 +0100

    commen

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ client.on('message', async message => {
     if (message.content.substring(0, 1) === '!') {
         var args = message.content.substring(1).split(' ');
         var cmd = args[0];
-        //prueba
 
         args = args.splice(1);
 
