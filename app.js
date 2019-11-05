3ac6b34e915254db33873a8d61df8def2a86f55f
Miguel Ángel Navarro Vera <manavarro@encamina.com>
2019-11-05 21:41:21 +0100

    añadido bloqueo a moxo

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var port = process.env.PORT || 666;
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
+const rich = new Discord.RichEmbed();
+
+const {
+    prefix,
+    token
+} = require('./auth.json');
+
 var Table = require('easy-table');
 const https = require('https');
 const xml2js = require('xml2js');
@@ -28,7 +34,7 @@ client.on('ready', () => {
 
 client.on('message', async message => {
 
-    if (message.content.substring(0, 1) === '!') {
+    if (message.content.substring(0, 1) === prefix) {
         var args = message.content.substring(1).split(' ');
         var cmd = args[0];
 
@@ -40,20 +46,63 @@ client.on('message', async message => {
                     message.reply("Introduce un comando valido");
                 }
 
-                var dkps = await GetDkp(args[0]);
-                var name = dkps.playername;
-                var wowClass = dkps.class;
-                var total = dkps.total;
-                var hours = dkps.hours;
-                var spent = dkps.spent;
-                var net = dkps.net;
-                message.reply(`${name}: Class:${wowClass} Dkps Actuales: ${net} Total Dkps: ${total} Dkps Gastados: ${spent} Horas:${hours}`);
+                console.log(message.member.user.username, message.member.user.discriminator);
+
+                if (message.member.user.username.toLocaleLowerCase() === 'moxo' && parseInt(message.member.user.discriminator) === 5478) {
+                    message.reply("panete", {
+                        files: ['https://thumb-p3.xhcdn.com/a/XQrKgHT7b2ZMrSS1i7dIhw/000/331/794/313_1000.gif']
+                    })
+                } else {
+                    if (args[0] !== "all") {
+                        var dkps = await GetDkp(args[0]);
+                        var name = dkps.playername;
+                        var wowClass = dkps.class;
+                        var rank = dkps.rank;
+                        var total = dkps.total;
+                        var hours = dkps.hours;
+                        var spent = dkps.spent;
+                        var net = dkps.net;
+                        message.reply(`${name}: Class:${wowClass} Dkps Actuales: ${net} Total Dkps: ${total} Dkps Gastados: ${spent} Horas:${hours}`);
+                    } else {
+                        var t = new Table;
+                        var dkpsAll = await GetDkp(args[0]);
+
+                        var playerList = '';
+                        var dkpList = '';
+                        var dkpTotalList = '';
+                        var classList = '';
+
+                        dkpsAll.forEach(function (dkpCharacter) {
+
+                            var parsedResult = dkpCharacter["ATTR"];
+                            var name = parsedResult.playername;
+                            var wowClass = parsedResult.class;
+                            var rank = parsedResult.rank;
+                            var total = parsedResult.total;
+                            var hours = parsedResult.hours;
+                            var spent = parsedResult.spent;
+                            var net = parsedResult.net;
+
+                            if (parseInt(net) != 0) {
+                                playerList += `${name}\n`;
+                                dkpList += `${net}\n`;
+                                dkpTotalList += `${total}\n`;
+                                classList += `${wowClass}\n`;
+                            }
+
+                        });
+
+                        sendEmbed(message, playerList, dkpList, dkpTotalList, classList);
+                    }
+                }
                 break;
         }
+
+
     }
 });
 
-client.login(auth.token);
+client.login(token);
 
 function GetDataFromDkpAzure(character) {
     return new Promise((resolve, reject) => {
@@ -66,13 +115,9 @@ function GetDataFromDkpAzure(character) {
                 });
 
                 res.on('end', function () {
-                    console.log('data', data);
                     parser.parseString(data, function (err, result) {
-
-                        console.log('FINISHED', err, result);
-
                         var roster = result['QDKP2EXPORT-DKP']['PLAYER'];
-
+                        roster.sort((a, b) => a['ATTR'].playername.localeCompare(b['ATTR'].playername));
                         if (character !== "all") {
                             var playerResult = roster.forEach((item) => {
                                 if (item['ATTR'].playername.toLowerCase() === `${character}-Mandokir`.toLowerCase()) {
@@ -97,4 +142,31 @@ async function GetDkp(character) {
     } catch (err) {
         console.log(err);
     }
+}
+
+function sendEmbed(message, playerList, dkpList, dkpTotalList, classList) {
+    message.channel.send({
+        embed: {
+            color: 3447003,
+            title: "Over DKP List:",
+            fields: [{
+                    name: "Player",
+                    value: playerList,
+                    inline: true
+                },
+                {
+                    name: "Class",
+                    value: classList,
+                    inline: true
+                },
+                {
+                    name: "Dkp (Net)",
+                    value: dkpList,
+                    inline: true
+                }
+            ],
+            timestamp: new Date()
+        }
+    });
+
 }
